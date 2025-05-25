a9f230ebd9f7ecfda20e475ed4b08b5aa589f23b
mkanderson2024 <kalani2014.ma@gmail.com>
2025-05-25 15:20:11 -0600

    Created customizable alert list for the website

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from './ProductData.mjs'
 import ProductList from './ProductList.mjs'
+import Alert from './Alert.mjs'
 
 // Create data source instance with your existing ProductData
 const dataSource = new ProductData('tents')
@@ -10,3 +11,9 @@ const tentListElement = document.querySelector('.product-list')
 // Create and initialize the ProductList
 const tentList = new ProductList('tents', dataSource, tentListElement)
 tentList.init()
+
+// Create and initialize the Alert
+
+const reader = new Alert("./json/alerts.json")
+
+init(Alert);
